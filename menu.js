87c20e113ca6b6f15f42059ec4fb8e05c6820a0b
Alexander Hirlam <alexander.hirlam@netmatters.local>
2018-10-11 11:43:00 +0100

    The menu is mostly styled and has been added on the right side of the page. Written some JS so that when the menu button is clicked the class menu-active is added as a class to the main body, this does not currently do anything more

--- menu.js
+++ menu.js
@@ -1,5 +1,15 @@
 
-function (showSideBar) {
 
+var sidebarShowing = false;
 
+document.getElementById("showSideBar").onclick = function() {sidebarFunction()};
+
+function sidebarFunction() {
+  if (sidebarShowing === false) {
+      $('body').addClass('menu-active');
+      sidebarShowing = true;
+    } else {
+      $('body').removeClass('menu-active');
+      sidebarShowing = false;
+    }
 }
