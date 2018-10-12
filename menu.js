82e4937c3d5f1a25a72a0ec8d3fb95156a1d488a
Alexander Hirlam <alexander.hirlam@netmatters.local>
2018-10-12 10:16:01 +0100

    make the menu button function correctly along with the overlay

--- menu.js
+++ menu.js
@@ -9,14 +9,14 @@ var afterStatus = false;
 
 
 document.getElementById("showSideBar").onclick = function() {sidebarFunction()};
-document.getElementById(".overlay").onclick = function() {afterExit()};
+document.getElementById("overlay").onclick = function() {afterExit()};
 //document.querySelector(".main"), '::after'.onclick = function() {afterExit()};
 
 function sidebarFunction() {
   if (sidebarShowing === false) {
       $('body').addClass('menu-active');
       sidebarShowing = true;
-      $('.overlay').addClass('sidebarOverlay');
+      $('#overlay').addClass('sidebarOverlay');
       console.log(sidebarShowing);
     } else {
       $('body').removeClass('menu-active');
@@ -27,8 +27,9 @@ function sidebarFunction() {
 
 function afterExit() {
   if (sidebarShowing === true) {
-    $('.overlay').removeClass('sidebarOverlay');
-    afterStatus = true;
+    $('#overlay').removeClass('sidebarOverlay');
+    $('body').removeClass('menu-active');
+    sidebarShowing = false;
     console.log(afterStatus);
   }
   console.log(afterStatus);
